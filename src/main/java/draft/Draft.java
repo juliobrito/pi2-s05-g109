78d2ce0446bdc34f052ff75c4e8040f25ac5b7df
julio <brito.julioc@gmail.com>
2022-05-12 15:50:57 -0300

    Get started. Draft files.

---
+++ src/main/java/draft/Draft.java
@@ -0,0 +1,12 @@
+package draft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Draft {
+
+    public static void main(String[] args) { SpringApplication.run(Draft.class, args);
+    }
+
+}
---
+++ src/test/java/draft/DraftTest.java
@@ -0,0 +1,13 @@
+package draft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DraftTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
